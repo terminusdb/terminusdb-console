45a55d62c74a4e1d088fac485fe94a56b948ca7c
Kitty Jose <kitty@datachemist.com>
2020-05-07 16:31:15 +0100

    removing dist and making sure no pass/ key included in file

--- console/localSettings.js
+++ console/localSettings.js
@@ -1,10 +1,10 @@
 
 console.log("____TERMINUSDB_SERVER",process.env.TERMINUSDB_SERVER);
 
-const server=process.env.TERMINUSDB_SERVER || "http://195.201.12.87:6380/"
+const server=process.env.TERMINUSDB_SERVER || "http://localhost:6363"
 const key=process.env.TERMINUSDB_KEY || "root"
 const db=process.env.TERMINUSDB_DB || ""
 
-export const localSettings = {server : server,//"http://localhost:6363",
+export const localSettings = {server : server,
                               key : key,
                               db : db}
