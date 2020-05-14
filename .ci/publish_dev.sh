dbf75fcfd3bc72e7422c5259c755f51db5452f8a
Robin de Rooij <rderooij685@gmail.com>
2020-05-14 16:20:35 +0200

    ci: create directory

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -8,4 +8,4 @@ VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
 npm unpublish "@terminusdb/terminusdb-console@$VERSION" || true
 npm publish
 echo "console.log('hello world!');" > terminusdb-console.min.js
-curl -T terminusdb-console.min.js -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/terminusdb-console-$VERSION.min.js?publish=1&override=1"
+curl -T terminusdb-console.min.js -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/$VERSION/terminusdb-console-$VERSION.min.js?publish=1&override=1"
