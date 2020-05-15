1117f32979638aaa07fda6591a66eebcb7952a47
Robin de Rooij <rderooij685@gmail.com>
2020-05-15 23:40:16 +0200

    ci: fix wrong env variable

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -5,7 +5,7 @@ _auth=$BINTRAY_TOKEN
 always-auth=true
 email=robin@datachemist.com" > $TRAVIS_BUILD_DIR/.npmrc
 VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
-curl -XDELETE -u"rrooij:$BINTRAY_CURL_TOKEN" "https://api.bintray.com/packages/terminusdb/npm-dev/terminusdb:terminusdb-console"
+curl -XDELETE -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/packages/terminusdb/npm-dev/terminusdb:terminusdb-console"
 npm publish
 curl -T "console/dist/terminusdb-console-main.css" -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/$VERSION/terminusdb-console-main.css?publish=1&override=1"
 curl -T "console/dist/terminusdb-console.min.js" -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/$VERSION/terminusdb-console.min.js?publish=1&override=1"
