416861083005e0e86d4e0f185310c4468dc7ccb9
Robin de Rooij <rderooij685@gmail.com>
2020-05-15 11:53:14 +0200

    ci: try to upload everything in dist

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -7,5 +7,9 @@ email=robin@datachemist.com" > $TRAVIS_BUILD_DIR/.npmrc
 VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
 npm unpublish "@terminusdb/terminusdb-console@$VERSION" || true
 npm publish
-curl -T console/dist/terminusdb-console.min.js -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/$VERSION/terminusdb-console.min.js?publish=1&override=1"
-curl -T console/dist/terminusdb-console-main.css -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/$VERSION/terminusdb-console-main.css?publish=1&override=1"
+
+for FILE in $(find console/dist -type f)
+do
+    UPLOAD_FILENAME=$(echo $FILE | sed 's,console/dist/,,')
+    curl -T "$FILE" -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/$VERSION/$UPLOAD_FILENAME?publish=1&override=1"
+done
