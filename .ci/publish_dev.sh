f354d285dd968a59067863362515304c1dbc11d7
Robin de Rooij <rderooij685@gmail.com>
2020-05-15 11:30:34 +0200

    ci: publish css as well

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -8,3 +8,4 @@ VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
 npm unpublish "@terminusdb/terminusdb-console@$VERSION" || true
 npm publish
 curl -T console/dist/terminusdb-console.min.js -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/$VERSION/terminusdb-console.min.js?publish=1&override=1"
+curl -T console/dist/terminusdb-console-main.css -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$VERSION/$VERSION/terminusdb-console-main.css?publish=1&override=1"
