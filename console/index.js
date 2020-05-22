f3922aaccb0c98741f0a4d20f0885f3b20a694ed
Kevin Feeney <kevin@datachemist.com>
2020-05-22 19:28:16 +0100

    fixing pathname / href problem that auth0 does not seem to like

--- console/index.js
+++ console/index.js
@@ -19,7 +19,7 @@ ReactDOM.render(
     <Auth0Provider
       domain={config.domain}
       client_id={config.clientId}
-      redirect_uri={window.location.href} //{`${window.location.origin}/download`}
+      redirect_uri={window.location.origin + window.location.pathname} //{`${window.location.origin}/download`}
       onRedirectCallback={onRedirectCallback}
       audience={config.audience}
     > 
