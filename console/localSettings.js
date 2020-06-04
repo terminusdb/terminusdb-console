0924a304f9f684bae268c1e68f1a26f08f8e05cb
Kevin Feeney <kevin@datachemist.com>
2020-06-04 17:28:21 +0100

    fixed error in localsettings

--- console/localSettings.js
+++ console/localSettings.js
@@ -7,12 +7,9 @@ let key=process.env.TERMINUSDB_KEY || user0bj.password
 
 const userName=user0bj.username || 'admin'
 
-const db=process.env.TERMINUSDB_DB || process.env.TERMINUSDB_SERVER
 
 if(!key){
 	key=window.sessionStorage.getItem("apiKey");
 }
 
-export const localSettings = {server : server,
-                              key : key,  
-                              db : db}
+export const localSettings = {server : server, key : key}
