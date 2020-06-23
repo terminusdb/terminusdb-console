5d85820d7b2b7514b23c91390d140ff17ae2e988
Robin de Rooij <rderooij685@gmail.com>
2020-06-23 13:54:14 +0200

    ci: use npm run serve instead of npm start
    npm start uses local dependencies

--- .ci/run_integration_tests.sh
+++ .ci/run_integration_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/terminusdb/terminusdb-quickstart.git
 terminusdb-quickstart/terminusdb-container run
-(npm run start&); sleep 15; npx cypress run
+(npm run serve&); sleep 15; npx cypress run
