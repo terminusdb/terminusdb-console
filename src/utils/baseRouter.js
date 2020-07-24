4eba274d14f394c3c99aa9d38e14945dde787126
francesca@datachemist.com <francesca@datachemist.com>
2020-07-24 10:46:40 +0100

    redirect_url

--- src/utils/baseRouter.js
+++ src/utils/baseRouter.js
@@ -1,5 +1,5 @@
 const TERMINUSDB=window.TERMINUSDB || {}
 const base_router_win=TERMINUSDB.base_router || null
 
-export const base_router = base_router_win || process.env.TERMINUSDB_APP_BASE_ROUTER || '/console';
+export const base_router = base_router_win || process.env.TERMINUSDB_APP_BASE_ROUTER || '/console/';
 export const redirect_uri=`${window.location.origin}${base_router}`
