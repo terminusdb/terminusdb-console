089b67af6c1cff4c1752200a0f9b9a06fed67872
francesca@datachemist.com <francesca@datachemist.com>
2020-07-29 18:02:20 +0100

    add timeline

--- src/components/History/HistoryNavigatorTimeline.js
+++ src/components/History/HistoryNavigatorTimeline.js
@@ -6,7 +6,10 @@ export const HistoryNavigatorTimeline = ({woqlClient}) => {
 
 	const { setConsoleTime, consoleTime, setHead, branch, ref, DBInfo, branches} = DBContextObj();
 
+	const setCurrentItem=(item)=>{
+		setHead(branch,item.commit);
+	}
 
-	return <div className="history__nav"><TimelineCommits branch={branch} woqlClient={woqlClient} setHead={setHead}/></div>
+	return <div className="history__nav"><TimelineCommits branch={branch} woqlClient={woqlClient} setHead={setCurrentItem}/></div>
 
 }
