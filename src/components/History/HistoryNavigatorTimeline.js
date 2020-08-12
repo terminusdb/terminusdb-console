166cef861e0d0c741e191bcfccb9b2ad7b12bce7
Kevin Feeney <kevin@datachemist.com>
2020-08-12 10:41:24 +0100

    tweaks to history timeline thing

--- src/components/History/HistoryNavigatorTimeline.js
+++ src/components/History/HistoryNavigatorTimeline.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {TimelineCommits} from '@terminusdb/terminusdb-react-components';
 import { DBContextObj} from "../Query/DBContext"
 
@@ -7,9 +7,33 @@ export const HistoryNavigatorTimeline = ({woqlClient}) => {
 	const { setConsoleTime, consoleTime, setHead, branch, ref, DBInfo, branches} = DBContextObj();
 
 	const setCurrentItem=(item)=>{
-		setHead(branch,item.commit);
+        console.log(item)
+		alert(branch)
+        setHead(branch,item.commit)
 	}
+    if(!branches || !DBInfo) return null
+    
+    console.log(DBInfo)
+    const [currentHead, setCurrentHead] = useState(null);
 
-	return <div className="history__nav"><TimelineCommits branch={branch} woqlClient={woqlClient} setHead={setCurrentItem}/></div>
+    useEffect(() => {
+        if((ref || branch) && branches){
+            if(ref){
+                setCurrentHead(ref)
+            }
+        }
+    }, [ref, branch, branches])
+
+    if(!branches) return null
+    return <div className="history__nav">
+        <TimelineCommits 
+            branch={branch}
+            woqlClient={woqlClient} 
+            setHead={setCurrentItem}
+            headMessage="Set Console Head to this Commit"
+            currentCommit={currentHead}
+            currentStartTime={consoleTime}    
+        />
+    </div>
 
 }
