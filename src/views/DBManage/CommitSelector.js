9565afec128ea69cc0fbddb19895d5250e386ec4
Kevin Feeney <kevin@datachemist.com>
2020-08-13 09:12:39 +0100

    added commit selector widget

---
+++ src/views/DBManage/CommitSelector.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react'
+import {TCForm} from '../../components/Form/FormComponents'
+import {CREATE_BRANCH_FORM, BRANCH_SOURCE_FORM} from './constants.dbmanage'
+import {
+    TERMINUS_SUCCESS,
+    TERMINUS_ERROR,
+    TERMINUS_INFO,
+    TERMINUS_COMPONENT,
+} from '../../constants/identifiers'
+import {TerminusDBSpeaks} from '../../components/Reports/TerminusDBSpeaks'
+import {printts} from '../../constants/dates'
+import {TimelineCommits} from '@terminusdb/terminusdb-react-components';
+import {BranchSelector} from '../../components/History/BranchSelector' 
+
+
+export const CommitSelector = ({branch, onSelect, commit, woqlClient, firstCommit, time, actionMessage}) => {
+    function setCurrentItem(item){
+        onSelect(item.commit)
+    }
+
+    function changeBranch(mybranch){
+        setMyBranch(mybranch)
+    }
+
+    let [myBranch, setMyBranch] = useState(branch)
+
+    return (<span>
+            <BranchSelector onChange={changeBranch} currentBranch={myBranch}/>             
+            <TimelineCommits 
+            branch={myBranch}
+            woqlClient={woqlClient} 
+            setHead={setCurrentItem}
+            headMessage={actionMessage}
+            currentCommit={commit}
+            firstCommit={firstCommit}
+            currentStartTime={time}    
+        />
+        </span>
+    )
+}
