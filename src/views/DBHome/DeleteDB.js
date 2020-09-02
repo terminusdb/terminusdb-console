d95d651b9a95ca950471a26c7d7f5a05b870b7c7
Kitty Jose <kitty@datachemist.com>
2020-09-02 14:17:38 +0100

    formatting delete local database text

--- src/views/DBHome/DeleteDB.js
+++ src/views/DBHome/DeleteDB.js
@@ -89,8 +89,8 @@ export const DeleteDB = ({meta}) => {
                 <ModalBody>
                     <Row key="rd">
                         <Col md={12} className="delete-modal-col-align">
-                            <span className="delete-modal-text"> This will delete your local database but will not effect the linked remote which will still be available in your hub account.
-                            {DELETE_DB_MODAL.message} {woqlClient.db()} </span>
+                            <div className="delete-modal-text"> This will delete your local database but will not effect the linked remote which will still be available in your hub account.</div><br/>
+                            <div>{DELETE_DB_MODAL.message} <b>{woqlClient.db()}</b></div>
                         </Col>
                     </Row>
 
