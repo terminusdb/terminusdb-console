4d42ee7613ba5f48cc65f51a5fba8f486f6cb5fe
Kitty Jose <kitty@datachemist.com>
2020-12-09 11:35:12 +0000

    adding class cycle and class inheritance error

--- src/components/Reports/SystemError.js
+++ src/components/Reports/SystemError.js
@@ -3,16 +3,18 @@ import { SYSTEM_ERROR, SYSTEM_ERROR_CSS, HIDE_ERROR_DETAILS, SHOW_ERROR_DETAILS,
     SYSTER_ERROR_MSG_CSS, SYSTER_ERROR_TITLE_CSS, UNKNOWN_ERROR } from "./constants.reports"
 import * as vios from "./constants.vios"
 import TerminusClient from '@terminusdb/terminusdb-client'
+import {WOQLClientObj} from '../../init/woql-client-instance'
 
 export const SystemError = ({error}) => {
     let msg="", msgObject = {}
+    const {woqlClient} = WOQLClientObj()
     const [showFull, setFull] = useState(false)
 
     if(error.data && error.data["system:message"]){
         msg = (error.data["system:message"]["@value"] ? error.data["system:message"]["@value"] : error.data["system:message"])
     }
 
-    function processErrorWitness (wit){
+    function parseWitnesses (wit){
         let obj={}
         wit.map(item => {
             for (var key in item) {
@@ -47,6 +49,7 @@ export const SystemError = ({error}) => {
                             obj.subject=`${TerminusClient.UTILS.shorten(val)}`
                         break
                     case vios.WITNESS_PARENT_TYPE:
+                        obj.parent=`${TerminusClient.UTILS.shorten(item[key]["@value"])}`
                         break
                     default:
                         console.log("Invalid Witness key " + key + " found in SystemError.js")
@@ -57,7 +60,7 @@ export const SystemError = ({error}) => {
         return obj
     }
 
-    function processApiMessage(apiMsg) {
+    function parseAPIMessage(apiMsg) {
         if(apiMsg.includes(vios.VIOLATION_WOQL_SYNTAX_ERROR))
             return "Syntax error found"
         else if (apiMsg.includes(vios.VIOLATION_KEY_HAS_UNKNOWN_PREFIX))
@@ -65,53 +68,100 @@ export const SystemError = ({error}) => {
         else return apiMsg
     }
 
-    if(error.data && error.data["api:message"]){
-        msg=processApiMessage(error.data["api:message"])
-        if(error.data["api:error"]["api:witnesses"]) {
-            let wit=error.data["api:error"]["api:witnesses"]
-            msgObject=processErrorWitness(wit)
+    function constructDataTypeObjectVio (msgObject) {
+        let text=""
+        text=text + ". " + msgObject.literal + " is " + msgObject.message + " for "
+        if(msgObject.property) {
+            text=text + " Property " + `${TerminusClient.UTILS.shorten(msgObject.property)}`
+        }
+        text=text + " of type " + msgObject.baseType
+        return text
+    }
 
+    function constructUntypedInstanceVio(msgObject) {
+        let text=""
+        text=text + " for "
+        if(msgObject.property) {
+            text=text + " Property " + `${TerminusClient.UTILS.shorten(msgObject.property)}`
+        }
+        text=text + ". " + msgObject.message["@value"]
+        return text
+    }
+
+    function constructDataTypeSubsumtionVio (msgObject) {
+        let text=""
+        text=text + " for document " + msgObject.subject + ". "
+        if(msgObject.property) {
+            text=text + "Expecting type " + msgObject.baseType + " for property " + msgObject.property + ". "
+        }
+        return text
+    }
+
+    function constructClassVio (msgObject) {
+        let m=msgObject.message["@value"], rep="", text=""
+        if(m.includes(msgObject.class)) {
+            rep=m.replace(msgObject.class, `${TerminusClient.UTILS.shorten(msgObject.class)}`)
+        }
+        text=text + ". " + rep
+        return text
+    }
+
+    function constructPropertyVio (msgObject) {
+        let m=msgObject.message["@value"], rep="", text=""
+        if(m.includes(msgObject.property)) {
+            rep=m.replace(msgObject.property, `${TerminusClient.UTILS.shorten(msgObject.property)}`)
+        }
+        text=text + ". " + rep
+        return text
+    }
+
+    function constructClassCycleVio (msgObject) {
+        let text=""
+        text = text + ". " + msgObject.message["@value"]
+        return text
+    }
+
+    function constructDefaultVio (msgObject) {
+        let val=msgObject.message["@value"], text=""
+        if(val==undefined)
+            text= text + ". " + msgObject.message
+        else text= text + ". " + val
+        return text
+    }
+
+    if(error.data && error.data["api:message"]){
+        msg=parseAPIMessage(error.data["api:message"])
+
+        // parse witnesses
+        if(error.data["api:error"] && error.data["api:error"]["api:witnesses"]) {
+            let wit=error.data["api:error"]["api:witnesses"]
+            msgObject=parseWitnesses(wit)
             switch(msgObject.type){
                 case vios.VIOLATION_WITH_DATA_TYPE_OBJECT:
-                    msg=msg + ". " + msgObject.literal + " is " + msgObject.message + " for "
-                    if(msgObject.property) {
-                        msg=msg + " Property " + msgObject.property
-                    }
-                    msg=msg + " of type " + msgObject.baseType
+                    msg= msg + constructDataTypeObjectVio(msgObject)
                     break
                 case vios.VIOLATION_UNTYPED_INSTANCE:
-                    msg=msg + " for "
-                    if(msgObject.property) {
-                        msg=msg + " Property " + msgObject.property
-                    }
-                    msg=msg + ". " + msgObject.message["@value"]
+                    msg= msg + constructUntypedInstanceVio(msgObject)
                     break
                 case vios.VIOLATION_DATA_TYPE_SUBSUMPTION:
-                    msg=msg + " for document " + msgObject.subject + ". "
-                    if(msgObject.property) {
-                        msg=msg + "Expecting type " + msgObject.baseType + " for property " + msgObject.property + ". "
-                    }
+                    msg= msg + constructDataTypeSubsumtionVio(msgObject)
                     break
                 case vios.VIOLATION_INVALID_CLASS_VIOLATION:
-                    if(msgObject.class){
-                        let m=msgObject.message["@value"], rep=""
-                        if(m.includes(msgObject.class)) {
-                            rep=m.replace(msgObject.class, `${TerminusClient.UTILS.shorten(msgObject.class)}`)
-                        }
-                        msg=msg + ". " + rep
+                case vios.VIOLATION_CLASS_INHERITANCE:
+                    if(msgObject.class) {
+                        msg= msg + constructClassVio(msgObject)
                     }
                     break
                 case vios.VIOLATION_PROPERTY_WITH_UNDEFINED_DOMAIN:
                     if(msgObject.property){
-                        let m=msgObject.message["@value"], rep=""
-                        if(m.includes(msgObject.property)) {
-                            rep=m.replace(msgObject.property, `${TerminusClient.UTILS.shorten(msgObject.property)}`)
-                        }
-                        msg=msg + ". " + rep
+                        msg=msg + constructPropertyVio(msgObject)
                     }
                     break
+                case vios.VIOLATION_CLASS_CYCLE:
+                    msg=msg + constructClassCycleVio(msgObject)
+                    break
                 default:
-                    console.log("Invalid Vio type " + msgObject.type + " found in SystemError.js")
+                    msg = msg + constructDefaultVio(msgObject)
                     break
             }
         }
@@ -127,11 +177,6 @@ export const SystemError = ({error}) => {
 
     return (
         <div className={SYSTEM_ERROR_CSS}>
-            {/*SYSTEM_ERROR.title &&
-                <span className={SYSTER_ERROR_TITLE_CSS}>
-                    {SYSTEM_ERROR.title}
-                </span>
-            */}
             { msg && <span className={SYSTER_ERROR_MSG_CSS}>
                 {msg}
             </span>
