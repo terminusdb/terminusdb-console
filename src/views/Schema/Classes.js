2587d2398557db9e39f0e6e9b7ecbb689fa3f74c
Kevin Feeney <kevin@datachemist.com>
2020-12-11 17:37:36 +0000

    adding frames for classes

--- src/views/Schema/Classes.js
+++ src/views/Schema/Classes.js
@@ -17,6 +17,8 @@ import {TerminusDBSpeaks} from '../../components/Reports/TerminusDBSpeaks'
 import {GraphFilter} from './GraphFilter'
 import {TERMINUS_ERROR, TERMINUS_TABLE} from '../../constants/identifiers'
 import {ControlledTable} from '../Tables/ControlledTable'
+import { FrameViewer } from '@terminusdb/terminusdb-react-components';
+import {BiArrowBack, BiNetworkChart} from "react-icons/bi"
 
 
 export const Classes = (props) => {
@@ -24,7 +26,9 @@ export const Classes = (props) => {
     const [query, setQuery] = useState(getClassQuery(props.graph))
     const [report, setReport] = useState()
     const [empty, setEmpty] = useState(false)
+    const [frame, setFrame] = useState(false)
     const { woqlClient} = WOQLClientObj()
+    
     useEffect(() => {
         if (
             props.graph &&
@@ -40,13 +44,28 @@ export const Classes = (props) => {
         return TerminusClient.WOQL.lib().classes(null, null, gstr)
     }
 
+    
+    const getClassFrame = (docType) => {
+        woqlClient.getClassFrame(docType).then((cf) => setFrame(cf))
+    }
+
+    const showClass = (row) => {
+        if(row && row.original) {
+            let cid = row.original["Class ID"]
+            if(cid) getClassFrame(cid)
+        }
+    }
+
     const tabConfig= TerminusClient.View.table();
     tabConfig.column_order("Class ID", "Class Name", "Parents", "Children", "Abstract", "Description")
     tabConfig.column("Abstract").minWidth(50).width(80)
     tabConfig.column("Parents", "Children", "Description").width(200)
     tabConfig.pager("remote")
     tabConfig.pagesize(10)
+    tabConfig.row().click(showClass)
 
+    const docview = TerminusClient.View.document();
+    docview.selectDocument = getClassFrame
 
     return (
         <div className={TAB_SCREEN_CSS}>
@@ -58,11 +77,32 @@ export const Classes = (props) => {
                      {GraphFilter(SCHEMA_CLASSES_ROUTE, filter, props.onChangeGraph)}
                 </Col>
             </Row>
-            <ControlledTable 
-                limit={tabConfig.pagesize()} 
-                query={query} 
-                view={tabConfig} 
-            />
+            {!frame && 
+                <ControlledTable 
+                    limit={tabConfig.pagesize()} 
+                    query={query} 
+                    view={tabConfig} 
+                />
+            }
+            {frame && <>
+                <GoBackIcon onClose={function(){setFrame()}} />
+                <FrameViewer 
+                    classframe={frame}
+                    mode="view" 
+                    view={docview} 
+                    type="table" 
+                    client={woqlClient}
+                />            
+            </>}
         </div>
     )
+}
+
+
+export const GoBackIcon = ({onClose}) => {
+    return <span onClick={onClose} style={{fontSize: "2em"}}>
+        <span className="d-nav-icons" title={"Back to Class List"}>
+            <BiArrowBack className="db_info_icon_spacing"/>
+        </span>
+    </span>
 }
