927632b4c27b90e0d7b446a4034164823bf35382
Kitty Jose <54106287+KittyJose@users.noreply.github.com>
2021-02-03 17:38:27 +0000

    reset input box bug

--- src/views/DBManage/Reset.js
+++ src/views/DBManage/Reset.js
@@ -3,22 +3,26 @@ import {Row, Col} from "react-bootstrap"
 import {RESET_BRANCH_FORM} from "./constants.dbmanage"
 
 export const Reset = ({branch, key, onReset, commit}) => {
-	const [commitDescriptor, setCommitDescriptor]=useState(commit)
+
+	function setCommit() {
+		let commitDescriptor = document.getElementById("cmt-inp").value
+		onReset(branch, commitDescriptor)
+	}
 
 	return <Row className="new-branch" key={key}>
 		<Col className="branch-id-col" >
-			{commit && <input onBlur={(evt) => {setCommitDescriptor(evt.target.value)}}
+			{commit && <input id={"cmt-inp"}
 						className={RESET_BRANCH_FORM.commitDescriptor.inputElement.className}
 						type={RESET_BRANCH_FORM.commitDescriptor.inputElement.type}
 						value = {commit}/>
 			}
-			{!commit && <input onBlur={(evt) => {setCommitDescriptor(evt.target.value)}}
+			{!commit && <input id={"cmt-inp"}
 				className={RESET_BRANCH_FORM.commitDescriptor.inputElement.className}
 				type={RESET_BRANCH_FORM.commitDescriptor.inputElement.type}
 				placeholder = {RESET_BRANCH_FORM.commitDescriptor.inputElement.placeholder}/>}
 		</Col>
 		<Col>
-			<button type="submit" onClick={()=> onReset(branch, commitDescriptor)} className="mt-1 tdb__button__base tdb__button__base--green">
+			<button type="submit" onClick={setCommit} className="mt-1 tdb__button__base tdb__button__base--green">
 				{RESET_BRANCH_FORM.buttons.submitText}
 			</button>
 		</Col>
