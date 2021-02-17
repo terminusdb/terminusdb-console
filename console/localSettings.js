164c0a8a894e5e56d19571ea98129bae56c7a9d1
Robin de Rooij <rderooij685@gmail.com>
2021-02-17 12:57:00 +0100

    localSettings: fix formatting

--- console/localSettings.js
+++ console/localSettings.js
@@ -4,12 +4,12 @@ const server = localStorage.getItem("terminusdb-server-override") || process.env
 const TERMINUSDB=window.TERMINUSDB || {}
 const user0bj=TERMINUSDB.user || {}
 
-let key=process.env.TERMINUSDB_KEY || user0bj.password 
+let key=process.env.TERMINUSDB_KEY || user0bj.password
 
 const userName=process.env.TERMINUSDB_USER || user0bj.username || 'admin'
 
-const hub_url = process.env.TERMINUS_HUB_URL //|| "https://hub-dev-server.dcm.ist/" 
-const bff_url = process.env.TERMINUS_BFF_URL //|| "https://hub-dev.dcm.ist/" 
+const hub_url = process.env.TERMINUS_HUB_URL //|| "https://hub-dev-server.dcm.ist/"
+const bff_url = process.env.TERMINUS_BFF_URL //|| "https://hub-dev.dcm.ist/"
 
 if(!key){
 	key=window.sessionStorage.getItem("apiKey");
@@ -17,9 +17,9 @@ if(!key){
 
 //console.log("KEY..",key)
 
-export const localSettings = {server : server, 
-							  key : key, 
-							  user: userName, 
-							  remote: hub_url, 
-							  bff: bff_url}
+export const localSettings = {server : server,
+                              key : key,
+                              user: userName,
+                              remote: hub_url,
+                              bff: bff_url}
 
