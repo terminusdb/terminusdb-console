0d05097dbad8b44992e21043bfd37354e1b80187
francesca@datachemist.com <francesca@datachemist.com>
2021-02-18 11:44:37 +0000

    baserouter

--- src/utils/baseRouter.js
+++ src/utils/baseRouter.js
@@ -1,6 +1,6 @@
 const TERMINUSDB=window.TERMINUSDB || {}
 const base_router_win=TERMINUSDB.base_router || null
 
-export const base_router = localStorage.getItem("terminusdb-base-router") ||  base_router_win || process.env.TERMINUSDB_APP_BASE_ROUTER || '/console/';
+export const base_router = localStorage.getItem("terminusdb-base-router") ||  base_router_win || process.env.TERMINUSDB_APP_BASE_ROUTER || '';
 //export const redirect_uri=`${window.location.origin}${base_router}`
 export const redirect_uri=`${window.location.origin}`
