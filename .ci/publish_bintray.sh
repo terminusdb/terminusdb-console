f36c57f4824b3550730fef3798d6731823df2a4d
Robin de Rooij <rderooij685@gmail.com>
2021-08-03 15:23:28 +0200

    remove bintray

--- .ci/publish_bintray.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BRANCH=$1
-curl -u "rrooij:$BINTRAY_API_TOKEN" "https://api.bintray.com/npm/terminusdb/npm-$BRANCH/auth" > .npmrc
-curl -XDELETE "https://api.bintray.com/packages/terminusdb/npm-$BRANCH/terminusdb:terminusdb-console" -u "rrooij:$BINTRAY_API_TOKEN"
-npm publish
-curl -T "console/dist/terminusdb-console-main.css" -u"rrooij:$BINTRAY_API_TOKEN" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$BRANCH/$BRANCH/terminusdb-console-main.css?publish=1&override=1"
-curl -T "console/dist/terminusdb-console.min.js" -u"rrooij:$BINTRAY_API_TOKEN" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-console/$BRANCH/$BRANCH/terminusdb-console.min.js?publish=1&override=1"
